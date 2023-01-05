0952ce923adb56992cee4980cf3798e5563ba4c7
hady63gx <113755764+hady63gx@users.noreply.github.com>
2023-01-05 15:57:58 +0800

    Update PyMySQL_3_delete.py

--- connect with DB/PyMySQL_3_delete.py
+++ connect with DB/PyMySQL_3_delete.py
@@ -1,8 +1,8 @@
 import mysql.connector
 connection = mysql.connector.connect(host='localhost',
                                      database='python_mysql_class',
-                                     user='dba',
-                                     password='dbaMySQL80')
+                                     user='xxx',
+                                     password='xxxxxxx')
 my_cursor = connection.cursor()
 delete_commit = 'DELETE FROM drink WHERE product_id = %s'
 data = 'DR0005'
